30dc1fb50905a5330381726e610341f7c8e825df
KarunaChandrakapure <70442938+KarunaChandrakapure@users.noreply.github.com>
2025-11-25 07:24:42 +0530

    Add files via upload
    Chatbot using openai and ollama

---
+++ app_ollama.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACKING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Simple Q&A with Ollama"
+
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ('system','You are a helpful assistant. Please response to the queries'),
+        ('user','Question:{question}')
+    ]
+)
+
+def generate_response(question,llm,temperature,max_tokens):
+    llm=Ollama(model=llm)
+    output_parser=StrOutputParser()
+    chain=prompt|llm|output_parser
+    answer=chain.invoke({'question':question})
+    return answer
+
+st.title('Enhanced Q&A Chatbot with Ollama')
+
+llm=st.sidebar.selectbox("Select Open Source model",['llama2:latest','llama3.2:1b'])
+
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+
+max_tokens=st.sidebar.slider("Max Tokens", min_value=50,max_value=300,value=150)
+
+
+st.write("How Can I help you ")
+user_input=st.text_input("YOu :")
+
+if user_input:
+    response=generate_response(user_input,llm,temperature,max_tokens)
+    st.write(response)
+
+else:
+    st.write("Please provide the user input")    
